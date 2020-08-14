506469e49edda5c5969d49ec9be05e6f63497073
Randy <randypriv@gmail.com>
2020-08-14 15:35:54 +0800

    add external

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,23 @@
 import routes from './routes'
 const isProd = process.env.NODE_ENV === 'production'
 
+
+const scripts = [
+  '//cdn.jsdelivr.net/npm/codemirror@5.56.0/lib/codemirror.js',
+  '//cdn.jsdelivr.net/npm/codemirror@5.56.0/mode/markdown/markdown.js',
+  '//cdn.jsdelivr.net/npm/react@16.13.1/umd/react.production.min.js'
+]
+
+const externals = {}
+
+if (process.env.WEB_EXTERNAL === 'true') {
+  externals['react'] = 'window.React'
+  scripts.push('//cdn.jsdelivr.net/npm/react@16.13.1/umd/react.production.min.js')
+
+  externals['react-dom'] = 'window.ReactDOM'
+  scripts.push('//cdn.jsdelivr.net/npm/react-dom@16.13.1/umd/react-dom.production.min.js')
+}
+
 export default {
   title: 'Docmate',
 
@@ -22,10 +39,8 @@ export default {
     }
   },
 
-  scripts: [
-    { src: '//cdn.jsdelivr.net/npm/codemirror@5.56.0/lib/codemirror.js' },
-    { src: '//cdn.jsdelivr.net/npm/codemirror@5.56.0/mode/markdown/markdown.js' }
-  ],
+  externals,
+  scripts,
 
   styles: [
     '//cdn.jsdelivr.net/npm/codemirror@5.56.0/lib/codemirror.css',
---
+++ README.md
@@ -0,0 +1,18 @@
+# docmate-web
+
+## Build
+
+```bash
+yarn
+
+# development
+npm run dev
+
+# build
+npm run build
+```
+
+### Build Env
+
+- WEB_EXTERNAL `true` use external library
+- GQL_PATH
